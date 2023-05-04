ac26a244c47086675e221ffec1655364c0f6635e
AMIT KUMAR BISWAS <88819794+amitbiswas1999@users.noreply.github.com>
2023-05-04 17:11:22 +0530

    Add files via upload

--- pinecode_upsert/Program.cs
+++ pinecode_upsert/Program.cs
@@ -1,136 +1,69 @@
-﻿using System.ComponentModel;
-using System.Net;
-using System.Collections.Generic;
-using HtmlAgilityPack;
-using System;
-using System.IO;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
 
-
-internal class Program
-
+namespace PineconeExample
 {
-    
-    private static void Main(string[] args)
+    class Program
     {
-        string url="https://www.cloudcraftz.com/";
-        int level=3;
-
-        List<string> web_url = new List<string>();
-        web_url.Add(url);
-
-        var doc= GetDocument(url);
-
-        var Links=ExtractLinks(doc);
-    
-        var all_links=FilteredLink(web_url,level);
-
-    //    Console.WriteLine(String.Join(Environment.NewLine, all_links));
-
-        Linktotext(all_links);
-       
-    }
-
-
-    static List<String>FilteredLink (List<string>web_url,int level)
-    {
-        List<string>local_url=new List<string>();
-
-        local_url.AddRange(web_url);
-
-        while(level!=0)
-
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            foreach (var items in local_url.ToList())
+            // Set up the request payload
+            var payload = new
             {
-                var docs= GetDocument(items);
-                var new_links=ExtractLinks(docs);
-                foreach (var links in new_links)
+                vectors = new[]
                 {
-                    if(!web_url.Contains(links))
+                    new
                     {
-                        web_url.Add(links);
-                        local_url.Add(links);
+                        id = "c",
+                        values = new[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                        metadata = new
+                        {
+                            text = "amitbuswas"
+                        }
                     }
+                }
+            };
 
-                    local_url.Remove(links);
-                    
+            // Serialize the payload to a JSON string
+            var json = JsonConvert.SerializeObject(payload);
+
+            // Set up the HTTP request
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://test1-5a3ac21.svc.us-west1-gcp-free.pinecone.io/vectors/upsert"),
+                Headers =
+                {
+                    { "accept", "application/json" },
+                    { "Api-Key", "76459ca8-165f-492f-a052-65c9c85071b8" },
+                },
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+
+            // Send the HTTP request and get the response
+            using (var client = new HttpClient())
+            {
+                var response = await client.SendAsync(request);
+
+                // Check if the response was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Vector upserted successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Error upserting vector:");
+                    Console.WriteLine(response.StatusCode);
+                    Console.WriteLine(await response.Content.ReadAsStringAsync());
                 }
             }
 
-        level--;
+            // Wait for user input before closing the console window
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
-
-        return web_url; 
-
     }
-
-
-    static HtmlDocument GetDocument(string url)
-    {
-
-        HtmlWeb web = new HtmlWeb();
-        HtmlDocument doc = web.Load(url);
-        // return doc; 
-        return doc;
-    }
-
-    
-
-    static string Gettext(HtmlDocument doc)
-    {
-        string str=doc.DocumentNode.InnerText.Trim();
-        // string[] delimiters = new[] { "\r\n", "\r", "\n", "\n\n", "\n\n\n", "\n\n\n\n" };
-        // string[] lines = str.Split(delimiters, StringSplitOptions.None);
-
-        // string concatenatedString = string.Join("\n", lines);
-        string cleanedText = Regex.Replace(str, @"\s+", " ");
-        string outputtext = Regex.Replace(cleanedText, @"#\d+", "");
-        string cleanedtext = Regex.Replace(outputtext, @"[^0-9a-zA-Z]+", " ").Trim();
-        return cleanedtext ;
-    }
-    
-
-
-
-    static List<string> ExtractLinks(HtmlDocument doc)
-    {
-        List<string> hrefTags = new List<string>();
-        foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
-        {
-            HtmlAttribute att = link.Attributes["href"];
-            hrefTags.Add(att.Value);
-        }
-
-        List<string> filteredList = hrefTags.Where(s => s.Contains("www.cloudcraftz")).ToList();
-
-        return filteredList;
-        
-    }
-
-    static void Linktotext(List<String>links)
-    {
-        foreach(var items in links)
-        {
-            var doc= GetDocument(items);
-            var str=Gettext(doc);
-            string[] substrings = items.Split('/');
-            string elem=substrings [substrings.Length-2];
-            string txt=".txt";
-            string file_name=elem +txt;
-
-           
-            
-
-            using (StreamWriter writer = new StreamWriter(file_name))
-            {
-                writer.WriteLine(str);
-            }
-
-            Console.WriteLine("Text saved to file: " + elem);
-
-        }     
-      
-    }
-
 }
