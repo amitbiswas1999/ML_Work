fb501b6c5e8a4c18882c9261ddcfd303adbef166
AMIT KUMAR BISWAS <88819794+amitbiswas1999@users.noreply.github.com>
2023-05-05 10:37:20 +0530

    Add files via upload

---
+++ embedding/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Set up the HttpClient with the OpenAI API endpoint and your API key
+        var token="Bearer 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";
+        HttpClient httpClient = new HttpClient();
+        httpClient.BaseAddress = new Uri("https://api.openai.com/");
+        httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer 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");
+
+        // Set up the request body as a JSON string
+        string requestBody = @"{
+            ""input"": ""Hi this is amit"",
+            ""model"": ""text-embedding-ada-002""
+        }";
+
+        // Create the request content as a StringContent object
+        var requestContent = new StringContent(requestBody, System.Text.Encoding.UTF8, "application/json");
+
+        // Make the request to the embeddings endpoint
+        HttpResponseMessage response = await httpClient.PostAsync("v1/embeddings", requestContent);
+
+        // Get the response body as a string
+        string responseBody = await response.Content.ReadAsStringAsync();
+        JObject responseJson = JObject.Parse(responseBody);
+
+        JToken embeddings = responseJson["data"][0]["embedding"];
+
+
+
+        // Print the response body to the console
+        Console.WriteLine(embeddings.Count());
+    }
+}
